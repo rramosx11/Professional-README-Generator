d5fe5abbbd2b093bc252024ad7d255253f5b9fb2
rramosx11 <rodolforamosd11@gmail.com>
2022-08-03 19:20:47 -0400

    added the string that structures that markdown file

--- index.js
+++ index.js
@@ -2,14 +2,8 @@
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown.js");
 const fs = require("fs");
-const { rejects } = require("assert");
 // TODO: Create an array of questions for user input
 const questions = [
-  {
-    type: "input",
-    name: "title",
-    message: `What is the title of your project?`,
-  },
   {
     type: "input",
     name: "github",
@@ -27,8 +21,8 @@ const questions = [
     type: "input",
     name: "email",
     message: "What is your email address? (Required)",
-    validate: (githubInput) => {
-      if (githubInput) {
+    validate: (emailInput) => {
+      if (emailInput) {
         return true;
       } else {
         console.log("Please enter your email address!");
@@ -38,66 +32,18 @@ const questions = [
   },
   {
     type: "input",
-    name: "what",
-    message: "What is your project and what problem will it solve? (Required)",
-    validate: (whatInput) => {
-      if (whatInput) {
-        return true;
-      } else {
-        console.log("Please enter what your project is!");
-        return false;
-      }
-    },
+    name: "title",
+    message: `What is the title of your project?`,
   },
   {
     type: "input",
-    name: "why",
-    message: "Why did you create this project? (Required)",
-    validate: (whyInput) => {
-      if (whyInput) {
+    name: "description",
+    message: "Please write a short description of your project? (Required)",
+    validate: (descriptionInput) => {
+      if (descriptionInput) {
         return true;
       } else {
-        console.log("Please enter why you created this project!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "how",
-    message: "How will someone use this? (Required)",
-    validate: (howInput) => {
-      if (howInput) {
-        return true;
-      } else {
-        console.log("Please enter what your project is!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "installation",
-    message:
-      "Please provide step-by-step installation instructions for your project. (Required)",
-    validate: (installInput) => {
-      if (installInput) {
-        return true;
-      } else {
-        console.log("Please enter your installation instructions!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "Please provide instructions and examples for use. (Required)",
-    validate: (usageInput) => {
-      if (usageInput) {
-        return true;
-      } else {
-        console.log("Please enter your use instructions!");
+        console.log("Please write a short description of your project!");
         return false;
       }
     },
@@ -109,43 +55,26 @@ const questions = [
     choices: ["agpl", "apache", "mit", "no license"],
   },
   {
-    type: "confirm",
-    name: "confirmContributers",
-    message: "Would you like to allow other developers to contribute?",
-    default: true,
+    type: "input",
+    name: "install",
+    message: "What command should be run to install dependencies??",
+    default: "npm i",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "What command should be run to run tests?",
+    default: "npm test",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What does the user need to know about using the repo?",
   },
   {
     type: "input",
     name: "contribute",
-    message: "Please provide guidelines for contributing. (Required)",
-    when: ({ confirmContributers }) => {
-      if (confirmContributers) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-    validate: (contributerInput) => {
-      if (contributerInput) {
-        return true;
-      } else {
-        console.log("Please enter contributer guidelines!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "test",
-    message: "Please provide instructions on how to test the app. (Required)",
-    validate: (testInput) => {
-      if (testInput) {
-        return true;
-      } else {
-        console.log("Please enter your use test instructions!");
-        return false;
-      }
-    },
+    message: "What does the user need to know about contributing to the repo?",
   },
 ];
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,43 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+
+  ## [Description](#table-of-contents)
+
+  ${data.description}
+
+  ## Table-of-Contents
+
+  * [Installation](#description)
+  * [Usage](#description)
   
+  * [Contributing](#contribute)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## [Installation](#table-of-contents)
+
+  ${data.install}
+
+  ## [Usage](#table-of-contents)
+
+  ${data.usage}
+
+  ## [Contributing](#table-of-contents)
+
+  ${data.contribute}
+
+  ## [Tests](#table-of-contents)
+
+  ${data.tests}
+
+  ## [Questions](#table-of-contents)
+
+  Please contact me using the following links:
+  [GitHub](https://github.com/${data.github})
+  [Email: ${data.email}](mailto:${data.email})
 `;
 }
 
